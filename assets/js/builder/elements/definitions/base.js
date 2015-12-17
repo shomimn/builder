4f8cf252897ad5b3f8d0bb3f856b46b8ec3b8536
shomimn <shomimn@gmail.com>
2015-12-17 16:32:24 +0100

    added gmaps

--- assets/js/builder/elements/definitions/base.js
+++ assets/js/builder/elements/definitions/base.js
@@ -105,6 +105,19 @@ baseBuilderElements.push({
 });
 
 baseBuilderElements.push({
+	name: 'google map',
+	frameworks: ['base'],
+	nodes: ['div'],
+	html: '<iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>',
+	types: ['flow'],
+	validChildren: ['flow'],
+	category: 'components',
+	previewScale: '0.5',
+	scaleDragPreview: false,
+	icon: 'google'
+});
+
+baseBuilderElements.push({
 	name: 'blockqoute',
  	frameworks: ['base'],
  	nodes: ['blockqoute'],
