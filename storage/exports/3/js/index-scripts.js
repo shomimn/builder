d6c6013105dc11de1303e545143b82336c060008
shomimn <shomimn@gmail.com>
2015-12-19 21:30:59 +0100

    nm

--- storage/exports/3/js/index-scripts.js
+++ storage/exports/3/js/index-scripts.js
@@ -1,59 +1,45 @@
-jQuery(document).ready(function($) {
+var doughnutData = [
+	{
+		value: 70,
+		color:"#74cfae"
+	},
+	{
+		value : 30,
+		color : "#3c3c3c"
+	}
+];
+var myDoughnut = new Chart(document.getElementById("javascript").getContext("2d")).Doughnut(doughnutData);
 
-
-    /*======= Skillset *=======*/
-    
-    $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
-
-        $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-            
-        });
-
-    });
-    
-    /* Bootstrap Tooltip for Skillset */
-    $('.level-label').tooltip();
-    
-    /* jQuery RSS - https://github.com/sdepold/jquery-rss */
-    $("#rss-feeds").rss(
-    
-        //Change this to your own rss feeds
-        "http://feeds.feedburner.com/TechCrunch/startups",
-        
-        {
-        // how many entries do you want?
-        // default: 4
-        // valid values: any integer
-        limit: 3,
-        
-        // the effect, which is used to let the entries appear
-        // default: 'show'
-        // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-        effect: 'slideFastSynced',
-        
-        // outer template for the html transformation
-        // default: "<ul>{entries}</ul>"
-        // valid values: any string
-        layoutTemplate: "<div class='item'>{entries}</div>",
-        
-        // inner template for each entry
-        // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-        // valid values: any string
-        entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
-        
-        }
-    );
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
-
-
-});
+var doughnutData = [
+	{
+		value: 90,
+		color:"#74cfae"
+	},
+	{
+		value : 10,
+		color : "#3c3c3c"
+	}
+];
+var myDoughnut = new Chart(document.getElementById("bootstrap").getContext("2d")).Doughnut(doughnutData);
+var doughnutData = [
+	{
+		value: 65,
+		color:"#74cfae"
+	},
+	{
+		value : 35,
+		color : "#3c3c3c"
+	}
+];
+var myDoughnut = new Chart(document.getElementById("wordpress").getContext("2d")).Doughnut(doughnutData);
+var doughnutData = [
+	{
+		value: 50,
+		color:"#74cfae"
+	},
+	{
+		value : 50,
+		color : "#3c3c3c"
+	}
+];
+var myDoughnut = new Chart(document.getElementById("photoshop").getContext("2d")).Doughnut(doughnutData);
