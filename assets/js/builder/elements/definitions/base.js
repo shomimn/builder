05a437d31446c76eeceffec33b9df7c4030b0e49
shomimn <shomimn@gmail.com>
2015-12-21 15:20:24 +0100

    iframe

--- assets/js/builder/elements/definitions/base.js
+++ assets/js/builder/elements/definitions/base.js
@@ -107,7 +107,7 @@ baseBuilderElements.push({
 baseBuilderElements.push({
 	name: 'google map',
 	frameworks: ['base'],
-	nodes: ['div'],
+	nodes: ['iframe'],
 	html: '<iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>',
 	types: ['flow'],
 	validChildren: ['flow'],
